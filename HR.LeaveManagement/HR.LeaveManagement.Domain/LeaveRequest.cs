8e21d65a19c03c83d7da027abd75ec9a4ce98013
Hadi Hosseininejad <hadi.hosseininejad@bestsecret.com>
2023-07-05 08:49:11 +0200

    Add Entites

---
+++ HR.LeaveManagement/HR.LeaveManagement.Domain/LeaveRequest.cs
@@ -0,0 +1,16 @@
+﻿using HR.LeaveManagement.Domain.Common;
+
+namespace HR.LeaveManagement.Domain;
+
+public class LeaveRequest : BaseEntity
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public DateTime DateRequested{ get; set; } 
+    public string RequestComments{ get; set; } = string.Empty;
+    public LeaveType? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public bool Approved{ get; set; }=false;
+    public bool Canceled { get; set; }= false;
+    public string RequestingEmployeeId{ get; set; }=string.Empty;
+}
