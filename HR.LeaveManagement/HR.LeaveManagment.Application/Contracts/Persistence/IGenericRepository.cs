644a00116edaa5ef8b2e67e333f85cc53cdae216
Hadi Hosseininejad <hadi.hosseininejad@bestsecret.com>
2023-07-05 09:47:16 +0200

    clean up

---
+++ HR.LeaveManagement/HR.LeaveManagment.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using HR.LeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagment.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+}
